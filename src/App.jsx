5bad992a22d873e243f3e35da8d6f1178b8ffb2c
Hollister009 <reactive.andrew@yahoo.com.ua>
2022-03-21 22:51:30 +0200

    Small adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
+const CATEGORIES = ['Hats', 'Jackets', 'Sneakers', 'Womens', 'Mens'];
+
 const App = () => (
   <div className="categories-container">
-    <div className="category-container">
-      {/* <img /> */}
-      <div className="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop Now</p>
+    {CATEGORIES.map((cat) => (
+      <div key={cat} className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>{cat}</h2>
+          <button>Shop Now</button>
+        </div>
       </div>
-    </div>
-    <div className="category-container">
-      {/* <img /> */}
-      <div className="category-body-container">
-        <h2>Jackets</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      {/* <img /> */}
-      <div className="category-body-container">
-        <h2>Sneakers</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      {/* <img /> */}
-      <div className="category-body-container">
-        <h2>Womens</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      {/* <img /> */}
-      <div className="category-body-container">
-        <h2>Mens</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
+    ))}
   </div>
 );
 
