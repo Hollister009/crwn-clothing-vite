f2eb1991511936d90c83c63df339e79545b4576f
Hollister009 <reactive.andrew@yahoo.com.ua>
2022-04-29 00:16:37 +0300

    finished Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
+import Authentication from './pages/Authentication';
 
 const Shop = () => <h1>Shop</h1>;
 
@@ -12,7 +12,7 @@ const App = () => (
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-in" element={<Authentication />} />
       </Route>
     </Routes>
   </main>
--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,11 +1,12 @@
 import SignUpForm from '../../components/SignUpForm';
 import SignInForm from '../../components/SignInForm';
+import './Authentication.scss';
 
-const SignIn = () => (
-  <div style={{ display: 'flex' }}>
+const Authentication = () => (
+  <div className="authentication-container">
     <SignInForm />
     <SignUpForm />
   </div>
 );
 
-export default SignIn;
+export default Authentication;
